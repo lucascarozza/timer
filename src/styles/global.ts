6a5f215c2562d634bfc4e395b0d98444f2bba49e
Lucas Carozza <lucas.carozza@icloud.com>
2025-04-18 12:48:35 -0300

    Add styled-components with default theme and global styles, and update index.html

---
+++ src/styles/global.ts
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    cursor: default;
+  }
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${(props) => props.theme["green-500"]};
+  }
+
+  body {
+    background-color: ${(props) => props.theme["gray-900"]};
+    color: ${(props) => props.theme["gray-300"]};
+  }
+
+  body, input, textarea, button {
+    font-family: "Roboto", sans-serif;
+    font-size: 1rem;
+    font-weight: 400;
+  }
+
+  a, button {
+    cursor: pointer;
+  }
+
+  input, text-area {
+    cursor: text;
+  }
+`;
